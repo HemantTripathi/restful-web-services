f869cda1faa0f2f27cd4682a43057f3670ccc0a3
HemantTripathi <hemant.tripathi20@gmail.com>
2022-07-27 23:13:50 +0530

    JPA feature

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,19 +2,29 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+
+@Entity // we can make our class as Entity by using @Entity annotation
 public class User {
-	
+
+	@Id
+	@GeneratedValue
 	private Integer Id;
 	
-	@Size(min=2, message="Name should have atleast 2 characters")
+	@Size(min=2, message="Name should have at least 2 characters")
 	private String name;
 	
 	@Past
 	private Date birthdate;
-	
+
+	public User() {
+	}
+
 	public User(Integer id, String name, Date birthdate) {
 		super();
 		Id = id;
